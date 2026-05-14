f09104ba99677102fbe739116b749c5401f67339
David Kakon <199112387+ARMFS@users.noreply.github.com>
2026-05-14 08:31:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // 301 Redirect indicates a permanent move
-header("Location: https://cdn.armfs.com/memos/IFA_Tax_Shelter.pdf", true, 301);
+header("Location: /IFA_Tax_Shelter.pdf", true, 301);
 exit();
 ?>
